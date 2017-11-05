9062d2a51fcbf8b63b80689a476b53e3221beb0e
Naresh Kumar Nagulavancha <nnagulavancha@incomm.com>
2017-11-04 23:55:42 -0400

    Firestore: find applications

--- functions/src/api/applications/findApplications.js
+++ functions/src/api/applications/findApplications.js
@@ -2,26 +2,38 @@
  * @Author: Naresh Kumar Nagulavancha 
  * @Date: 2017-11-04 02:38:12 
  * @Last Modified by: Naresh Kumar Nagulavancha
- * @Last Modified time: 2017-11-04 22:04:33
+ * @Last Modified time: 2017-11-04 23:50:48
  */
 
 const admin = require('firebase-admin');
 
 module.exports = (req, res) => {
-    
-    const appID = req.params.app;
-    if(appID){
-        admin.database().ref('/applications/').orderByChild("name").once("value", (data) => {
-            res.status(200).send(data);
-        }, (error) => {
-            res.status(500).send(error);
-        });
-    } 
-    else{
-        admin.database().ref('/applications').once("value", (data) => {
-            res.status(200).send(data);
-        }, (error) => {
-            res.status(500).send(error);
-        }
-    );
-}};
+
+    const appName = req.params.app;
+    // firestore database
+    const db = admin.firestore().collection('applications');
+
+    if (appName) {
+        db.doc(appName).get()
+            .then(doc => {
+                if(!doc.exists){
+                    res.status(404).send('No document found');
+                }
+                else {
+                    res.send(doc.data());
+                }
+            })
+            .catch(error => res.status(500).send(error));
+    } else {
+        var docs = [];
+        db.get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    docs.push(doc.data());
+                });
+                res.send(docs);
+            }
+            )
+            .catch(error => res.status(500).send(error));
+    }
+};
