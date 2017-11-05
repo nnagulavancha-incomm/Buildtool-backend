a6fccd226b8e9108655a898a4ca85ec57ef90723
Naresh Kumar Nagulavancha <nnagulavancha@incomm.com>
2017-11-05 00:57:52 -0400

    Firestore: delete application

--- functions/src/api/applications/deleteApplication.js
+++ functions/src/api/applications/deleteApplication.js
@@ -2,9 +2,14 @@
  * @Author: Naresh Kumar Nagulavancha 
  * @Date: 2017-11-04 02:38:51 
  * @Last Modified by: Naresh Kumar Nagulavancha
- * @Last Modified time: 2017-11-04 02:41:30
+ * @Last Modified time: 2017-11-05 00:54:45
  */
 
+const admin = require('firebase-admin');
+
 module.exports = (req, res) => {
-    res.send('delete application executed');
+    const appName = req.params.app;
+    admin.firestore().collection('applications').doc(appName).delete()
+        .then(() => res.status(200).send('success'))
+        .catch(error => res.status(500).send(error));
 };
