87cb285c8670844eeaec239b57fa957edd78945f
Naresh Kumar Nagulavancha <nnagulavancha@incomm.com>
2017-11-05 01:31:01 -0400

    Firestore: update application

--- functions/src/api/applications/updateApplication.js
+++ functions/src/api/applications/updateApplication.js
@@ -2,9 +2,39 @@
  * @Author: Naresh Kumar Nagulavancha 
  * @Date: 2017-11-04 02:58:37 
  * @Last Modified by: Naresh Kumar Nagulavancha
- * @Last Modified time: 2017-11-04 02:59:01
+ * @Last Modified time: 2017-11-05 01:26:55
  */
 
+var admin = require('firebase-admin');
+
 module.exports = (req, res) => {
-    res.send('Update application execution started');
+    let appName = req.params.app;
+    let body = req.body;
+
+    let app = {
+        author: body.author,
+        name: body.name,
+        version: body.version,
+        uatAnalyticsId: body.uatAnalyticsId,
+        prodAnalyticsId: body.prodAnalyticsId,
+        mobileAppUat: body.mobileAppUat,
+        mobileAppProd: body.mobileAppProd,
+        mobileAppCmsId: body.mobileAppCmsId,
+        updatedDate: new Date(),
+        isAvailableForUse: body.isAvailableForUse,
+        androidBundleId: body.androidBundleId,
+        iosBundleId: body.iosBundleId
+    };
+    var ref = admin.firestore().collection('applications').doc(appName);
+    ref.update(app)
+        .then(() => {
+            ref.get()
+                .then(a => {
+                    if(a.exists){
+                        res.status(200).send(a.data());
+                    }
+                })
+                .catch(error => res.status(500).send(error));
+        })
+        .catch(error => res.status(500).send(error));
 };
