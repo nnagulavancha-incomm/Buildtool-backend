569915b7137df86ad37e46c42d0cc5eaa728619a
Naresh Kumar Nagulavancha <nnagulavancha@incomm.com>
2017-11-05 01:45:26 -0400

    Firestore: added cms validation

--- functions/src/api/applications/updateApplication.js
+++ functions/src/api/applications/updateApplication.js
@@ -2,7 +2,7 @@
  * @Author: Naresh Kumar Nagulavancha 
  * @Date: 2017-11-04 02:58:37 
  * @Last Modified by: Naresh Kumar Nagulavancha
- * @Last Modified time: 2017-11-05 01:26:55
+ * @Last Modified time: 2017-11-05 01:44:52
  */
 
 var admin = require('firebase-admin');
@@ -10,6 +10,23 @@ var admin = require('firebase-admin');
 module.exports = (req, res) => {
     let appName = req.params.app;
     let body = req.body;
+
+    var ref = admin.firestore().collection('applications').doc(appName);
+    
+    if(body.cmsNameChanged) {
+        
+        ref.get()
+            .then(a => {
+                var app = a.data();
+                var tempAssets = app.assets;
+                tempAssets = tempAssets.filter(e => {
+                    return e.name === "UatAES" || e.name === "ProdAES";
+                });
+                app.assets = tempAssets;
+                ref.update(app);
+            })
+            .catch(error => console.error(error));
+    }
 
     let app = {
         author: body.author,
@@ -25,7 +42,6 @@ module.exports = (req, res) => {
         androidBundleId: body.androidBundleId,
         iosBundleId: body.iosBundleId
     };
-    var ref = admin.firestore().collection('applications').doc(appName);
     ref.update(app)
         .then(() => {
             ref.get()
